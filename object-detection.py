ce4029ee74c9b65d938340620727e246b3839d7e
Piyush <Piyush.Dharkar@gmail.com>
2020-07-05 17:08:40 +0530

    Added comments.

--- object-detection.py
+++ object-detection.py
@@ -2,24 +2,29 @@ import cv2
 
 vid = cv2.VideoCapture('birds.mp4')
 
+# MIL tracker in open CV
 tracker = cv2.TrackerMIL_create()
 
 ok, frame = vid.read()
 
+# Prompt user to select region of interest
 bb = cv2.selectROI(frame, False)
 
 ok = tracker.init(frame, bb)
 
 while True:
+    # Capture frame
     ok, frame = vid.read()
     
     if not ok:
         print('Done')
         break
     
+    # Update tracker / optical flow
     can_track, bb = tracker.update(frame)
     
     if can_track:
+        # Draw new bounding box using coordinates obtained from tracker
         top_left = (int(bb[0]), int(bb[1]))
         bot_right = (int(bb[0] + bb[2]), int(bb[1] + bb[3]))
         cv2.rectangle(frame, top_left, bot_right, (0, 0, 255), 2)   
@@ -30,8 +35,10 @@ while True:
         print("Error tracking ROI")
         break
 
+    # Display frame
     cv2.imshow('Video', frame)
     
+    # Delay before drawing next frame
     k = cv2.waitKey(20) & 0xff
 
     if k == 27:
